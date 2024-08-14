035476865685cb725a6f5a72afadb12f56371292
EC2 Default User <ec2-user@ip-172-31-8-114.ap-south-1.compute.internal>
2024-08-14 07:40:29 +0000

    Files updated

---
+++ upload.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_FILES['file'])) {
+        $file = $_FILES['file'];
+        if ($file['error'] == UPLOAD_ERR_OK) {
+            $upload_dir = '/var/www/html/uploads/';
+            $upload_file = $upload_dir . basename($file['name']);
+            if (move_uploaded_file($file['tmp_name'], $upload_file)) {
+                echo 'File successfully uploaded.';
+            } else {
+                echo 'Failed to move uploaded file.';
+            }
+        } else {
+            echo 'File upload error: ' . $file['error'];
+        }
+    } else {
+        echo 'No file uploaded.';
+    }
+}
+?>
